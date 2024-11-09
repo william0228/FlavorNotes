c84ca1b15ca14a4645d79c59caae2af675905a20
william0228 <songyunwus@gmail.com>
2024-11-09 01:08:47 -0800

    Initial Commit

---
+++ FlavorNotes/FlavorNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorNotesApp.swift
+//  FlavorNotes
+//
+//  Created by 王嵩允 on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlavorNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
